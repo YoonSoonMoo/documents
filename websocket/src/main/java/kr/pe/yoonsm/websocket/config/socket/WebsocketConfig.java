a7b47a7d2523f4b27a54d67771a6e3883ee7521f
yoonsm <ysmb482@gmail.com>
2022-05-01 23:59:01 +0900

    client files added.

--- websocket/src/main/java/kr/pe/yoonsm/websocket/config/socket/WebsocketConfig.java
+++ websocket/src/main/java/kr/pe/yoonsm/websocket/config/socket/WebsocketConfig.java
@@ -15,15 +15,26 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompChannelInterceptor stompChannelInterceptor;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
+    /**
+     * websocket 연결
+     * @param stompEndpointRegistry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry){
         stompEndpointRegistry.addEndpoint("/websocket").withSockJS();
     }
+
+    /**
+     * topic 에 subscribe
+     * app 에 메시지를 송신
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
